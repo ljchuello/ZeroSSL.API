5be7420f531fa3a7ffdc9192816c25f41b97af6c
Leonardo Chuello <ljchuello@gmail.com>
2023-11-18 16:26:09 -0500

    Create complete

--- Test/Program.cs
+++ Test/Program.cs
@@ -14,11 +14,22 @@ namespace Test
 
         static async Task MainAsync()
         {
-            string dominio = $"{Guid.NewGuid()}.entecprois.com";
+            string dominio = $"101.entecprois.com";
 
             ZeroSslClient zeroSslClient = new ZeroSslClient(await File.ReadAllTextAsync("D:\\zerossltoken.txt"));
 
-            await zeroSslClient.Certificate.GetAsync("2d3add0243b8b17faf73fa176b60286a");
+            // Llave privada
+            string privateKey = zeroSslClient.Tools.GenerarClavePrivada();
+
+            // Certificado
+            Certificate certificate = await zeroSslClient.Certificate.Create(dominio, privateKey);
+
+            // Grabar fichero
+            FileInfo fileInfo = new FileInfo(certificate.Validation.OtherMethods.DomainDotCom.FileValidationUrlHttp);
+            string ficharo = $"{fileInfo}".Replace($"http://{dominio}", string.Empty);
+            ficharo = ficharo.Replace("/", "\\");
+            ficharo = $"C:\\inetpub\\wwwroot{ficharo}";
+            await File.WriteAllTextAsync(ficharo, $"{string.Join("\n", certificate.Validation.OtherMethods.DomainDotCom.FileValidationContent)}");
 
             
             Console.ReadLine();
--- ZeroSSLApi/Client/CertificateClient.cs
+++ ZeroSSLApi/Client/CertificateClient.cs
@@ -17,10 +17,33 @@ namespace ZeroSSLApi.Client
             _token = token;
         }
 
-        public async Task<Certificate> GetAsync(string certificateid)
+        public async Task<Certificate> Get(string certificateid)
         {
             // Get list
             string json = await Core.SendGetRequest($"/certificates/{certificateid}?access_key={_token}");
+
+            // To object
+            Certificate certificate = JsonConvert.DeserializeObject<Certificate>(json);
+
+            // Get DomainDotCom
+            JObject result = JObject.Parse(json);
+            certificate.Validation.OtherMethods.DomainDotCom = JsonConvert.DeserializeObject<DomainDotCom>($"{result["validation"]["other_methods"][certificate.CommonName]}") ?? new DomainDotCom();
+
+            // Return
+            return certificate;
+        }
+
+        public async Task<Certificate> Create(string domain, string privateKey)
+        {
+            // Csr
+            ToolsClient toolsClient = new ToolsClient(_token);
+            string csr = toolsClient.GenerarCSR(domain, privateKey);
+
+            // Set
+            string raw = $"{{ \"certificate_domains\": \"{domain}\", \"certificate_csr\": \"{csr.Trim()}\" }}";
+
+            // Send
+            string json = await Core.SendPostRequest($"/certificates?access_key={_token}", raw);
 
             // To object
             Certificate certificate = JsonConvert.DeserializeObject<Certificate>(json);
